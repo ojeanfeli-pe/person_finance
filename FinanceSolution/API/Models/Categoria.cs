5893b742cf11e5a330f120ba2499850a11cc5595
pablopasquim <pablo.pasquim7@gmail.com>
2025-04-24 15:07:10 -0300

    Deleted class

--- FinanceSolution/API/Models/Categoria.cs
+++
@@ -1,5 +0,0 @@
-namespace API.Models;
-public class Categoria {
-    public int Id { get; set; }
-    public string Nome { get; set; }
-}
--- FinanceSolution/API/Models/Usuario.cs
+++
@@ -1,6 +0,0 @@
-namespace API.Models;
-
-public class Usuario {
-    public int Id { get; set; }
-    public string Nome { get; set; }
-}
