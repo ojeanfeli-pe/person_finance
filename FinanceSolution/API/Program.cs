ff94b6dfaf144654e29234bf05a90832bb89c840
Nichele135 <141736358+Nichele135@users.noreply.github.com>
2025-04-25 13:27:40 -0300

    PUT e DELETE do usuario pronto

--- FinanceSolution/API/Program.cs
+++ FinanceSolution/API/Program.cs
@@ -4,15 +4,42 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    /*options.EnableAnnotations();*/
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        
+        Title = "Swagger Documentação Web API Finanças Pessoais",
+        Description = "EndPoints para gerenciar Finanças Pessoais",
+        Contact = new OpenApiContact(){
+            Name = "Jean Moreira, Andre Nichele, Pablo P",
+            Url = new Uri("https://github.com/ojeanfeli-pe/person_finance"),
+        },
+    
+        License = new OpenApiLicense(){
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+    }
+);
+
 var app = builder.Build();
 
-// Endopints
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Endopints dos usuarios
 
 // GET: users = Listar todos os users cadastrados
-app.MapGet("/users", 
+app.MapGet("/api/users", 
     ([FromServices] AppDataContext ctx) => 
     {
         if(ctx.Users.Any()){
@@ -22,7 +49,7 @@ app.MapGet("/users",
 });
 
 // Post: users/register = Registrar e salver um user
-app.MapPost("/users/register",
+app.MapPost("/api/users/register",
     ([FromBody] User user, [FromServices] AppDataContext ctx) =>
     {
         // Verifica se o nome já existe
@@ -41,6 +68,42 @@ app.MapPost("/users/register",
         ctx.SaveChanges();
 
         return Results.Created("", user);
+});
+
+// Atualiza o usuario e senha pelo ID
+
+app.MapPut("/api/users/{id}", ([FromRoute] int id, [FromBody] User user, [FromServices] AppDataContext ctx) =>{
+
+    User? userUpdate = ctx.Users.Find(id);
+
+     if(userUpdate.Name == null){
+        return Results.BadRequest("Usuario não existe!");
+    }
+
+
+   if   (userUpdate != null){
+        userUpdate.Name = user.Name;
+        userUpdate.Password = user.Password;
+        ctx.Users.Update(userUpdate);
+        ctx.SaveChanges();
+        return Results.Ok(userUpdate);
+   }
+   return Results.NotFound();
+
+});
+
+//Deletar usuario e senha pelo ID
+
+app.MapDelete("/api/users/{id}", ([FromRoute] int id, [FromServices] AppDataContext ctx) =>{
+    
+    User? user = ctx.Users.Find(id);
+    
+    if(user == null){
+        return Results.NotFound();
+    }
+    ctx.Users.Remove(user);
+    ctx.SaveChanges();
+    return Results.NoContent();
 });
 
 app.Run();
