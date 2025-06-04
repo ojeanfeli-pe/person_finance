64eaa61b407d9c656ab7ea2f22f496b6403f8b3a
Nichele135 <141736358+Nichele135@users.noreply.github.com>
2025-06-03 21:56:41 -0300

    adicionando um css basico, e crriando as classe app.css e header.css

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
-
-    return (
-        <header>
-            <nav>
-                <div >Transações</div>
-                        <li>
-                            <Link to="/pages/transacoes/listar">
-                                Lista de trasações
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/pages/transacoes/cadastrar">
-                                Cadastrar trasações
-                            </Link>
-                        </li>
-                </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <nav className="nav">
+        <h2 className="titulo">Finanças Pessoais</h2>
+        <ul className="nav-lista">
+          <li>
+            <Link to="/pages/transacoes/listar" className="nav-link">
+              Lista de Transações
+            </Link>
+          </li>
+          <li>
+            <Link to="/pages/transacoes/cadastrar" className="nav-link">
+              Cadastrar Transação
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
-
 
 export default Header;
