1963b9e67aa0f84071f8e6d5224aec946a28cbad
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2025-06-09 12:06:31 -0300

    feat: botão da Home redireciona para página de cadastro de transações

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="home-container">
       <h1>Seja bem-vindo à sua página de Finanças Pessoais</h1>
       <p>Organize seus gastos, visualize suas transações e mantenha o controle!</p>
-      <Link to="/listar-transacoes">
+      <Link to="/pages/transacoes/cadastrar">
         <button className="home-button">Abrir sua agenda</button>
       </Link>
     </div>
